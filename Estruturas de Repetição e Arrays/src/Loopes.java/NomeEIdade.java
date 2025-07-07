0fd7d819bb63af2239cb00aa770644317a873284
arthur <arthursantosstx2.0@gmail.com>
2025-07-07 14:49:33 -0300

    git commit

--- Estruturas de Repetição e Arrays/src/Loopes.java/NomeEIdade.java
+++ Estruturas de Repetição e Arrays/src/Loopes.java/NomeEIdade.java
@@ -1,11 +1,4 @@
-/*                💭 EXERCÍCIOS ESTRUTURAS DE REPETIÇÃO
-📝                          ( WHILE )
-*🔸Nome e Idade: Faça um programa que leia conjuntos de dois valores,
-* o primeiro representando o nome do aluno e o segundo representando a sua idade.
-*  (Pare o programa inserindo o valor 0 no campo nome)
- */
-
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class NomeEIdade {
     public static void main(String[] args) {
