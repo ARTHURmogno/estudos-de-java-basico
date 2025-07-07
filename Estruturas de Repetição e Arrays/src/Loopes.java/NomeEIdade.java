1f11ca2cf88d4c653a9f8d5d04881a3fb0159542
arthur <arthursantosstx2.0@gmail.com>
2025-07-07 14:00:12 -0300

    git commot

--- Estruturas de Repetição e Arrays/src/Loopes.java/NomeEIdade.java
+++ Estruturas de Repetição e Arrays/src/Loopes.java/NomeEIdade.java
@@ -11,10 +11,17 @@ public class NomeEIdade {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Qual, Seu Nome! ");
+        System.out.println("DIGITE o NOME do Aluno ( ou 0 para Sair): ");
         String nome = scanner.next();
 
-        System.out.println("Qual, Sua Idade! ");
-        int idade = scanner.nextInt();
+        while(!nome.equals("0")) {
+            System.out.println("Digite a idade de " + nome + ": ");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite o nome do próximo aluno ( ou 0 para Sair): ");
+            nome = scanner.next();
+        }
+        System.out.println("Programa Finalizado. ");
+        scanner.close();
     }
 }
